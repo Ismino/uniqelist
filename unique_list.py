576c3c97fb2bc994d51f183edb6b822e915bfaf6
Ismino <denise_cigel@hotmail.com>
2025-01-05 11:41:18 +0000

    add logic to program

--- unique_list.py
+++ unique_list.py
@@ -21,4 +21,20 @@ def create_unique_list_and_calculate_average():
         except ValueError:
             print("ogiltig inmatning. ange ett giltigt tal eller 'klar'.")
 
-        
+    
+    if unique_elements:
+        #konverterar mängden till en lista 
+        unique_list = list(unique_elements)
+        # beräknar genomsnittet 
+        average = sum(unique_list) / len(unique_list)
+
+        print(f"\n den slutliga listan med unika element : {unique_list}")
+        print(f"genomsnittet av de unika elementen är : {average:.2f}")
+    else:
+        print("ingen data tillgänlig för att beräkna genomsnittet")
+
+    
+# kör program
+
+if __name__ == "__main__":
+    create_unique_list_and_calculate_average()
